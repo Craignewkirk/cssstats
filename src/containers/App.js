91716a3e28cf380325d4abe85bc1f0f25e2b4de9
John Otander <johnotander@gmail.com>
2016-04-28 18:21:45 -0600

    Add better FontColorBlock example

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import { routeActions } from 'react-router-redux'
 
 import Header from '../components/Header'
-import FontColorBlock from '../components/FontColorBlock'
 
 function mapDispatchToProps (dispatch) {
   return {
--- src/containers/DesignSystem.js
+++ src/containers/DesignSystem.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react'
-import { Stat } from 'rebass'
 import { Flex, Box } from 'reflexbox'
 
 import FontColorBlock from '../components/FontColorBlock'
 
 class DesignSystem extends Component {
   render () {
+    const colors = [
+      '#BBCCBE', '#B59DA4', '#85756E', '#44420D', '#262626',
+      '#DB504A', '#FF6F59', '#254441', '#43AA8B', '#B2B09B',
+      '#FFBD72', '#3D9992', '#9BCCA1', '#36935B', '#1C4441'
+    ]
+    const fontColorBlocks = colors.map(color => {
+      return (
+        <Box col={2} p={2}>
+          <FontColorBlock color={color} />
+        </Box>
+      )
+    })
+
     return (
       <div>
        <div className='pv5 pv6-ns ph2 ph4-ns bg-near-white'>
@@ -13,20 +25,10 @@ class DesignSystem extends Component {
             <h1 className='f1 f-headline-ns mb0 pb0'>Design System</h1>
             <h2 className='f2 f1-ns i system-serif mid-gray'>The building blocks that make up CSS Stats</h2>
           </div>
-        </div> 
+        </div>
         <div className='pa3'>
-          <h3 className='code'>&lt;Stat label='tomato' color='tomato' value='Aa' /&gt;</h3>
-          <Flex wrap={true}>
-            <Box col={2}>
-              <Stat label='#444' color='#444' value='Aa' />
-            </Box>
-            <Box col={2}>
-              <Stat label='#292f33' color='#292f33' value='Aa' />
-            </Box>
-            <Box col={2}>
-              <Stat label='#1b95e0' color='#1b95e0' value='Aa' />
-            </Box>
-          </Flex>
+          <h3 className='code'>&lt;FontColorBlock color='tomato' /&gt;</h3>
+          <Flex wrap={true}>{fontColorBlocks}</Flex>
         </div>
       </div>
     )
