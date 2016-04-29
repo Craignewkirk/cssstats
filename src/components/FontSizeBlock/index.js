4c4b3c9c11ac7f2539a3de64aac242a183dd279f
John Otander <johnotander@gmail.com>
2016-04-28 19:52:38 -0600

    Add font size text to FontSizeBlock

--- src/components/FontSizeBlock/index.js
+++ src/components/FontSizeBlock/index.js
@@ -3,7 +3,7 @@ import React, { PropTypes } from 'react'
 const FontSizeBlock = ({ fontSize, className }) => {
   return (
     <div className={className}>
-      <p className='ma0 pa0' style={{fontSize}}>{fontSize}</p>
+      <p className='ma0 pa0' style={{fontSize}}>Font Size {fontSize}</p>
     </div>
   )
 }
