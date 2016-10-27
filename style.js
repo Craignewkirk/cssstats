ba0315c17cec77567026a36a8d71645c75b36f1c
John Otander <johnotander@gmail.com>
2016-10-26 22:55:36 -0600

    Fix formatting

--- style.js
+++ style.js
@@ -1,8 +1,8 @@
 const style = {
   fontFamily: '-apple-system, BlinkMacSystemFont, roboto, sans-serif',
-	lineHeight: 1.5,
-	color: '#222',
-	backgroundColor: '#fff'
+  lineHeight: 1.5,
+  color: '#222',
+  backgroundColor: '#fff'
 }
 
 export default style
